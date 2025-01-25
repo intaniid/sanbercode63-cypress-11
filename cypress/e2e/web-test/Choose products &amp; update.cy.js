00889d3c4cbdb6a781565a3b72cf46c24b5b67c3
intaniid <damayantiintani@gmail.com>
2025-01-25 13:57:37 +0700

    rapihkan baris

--- cypress/e2e/web-test/Choose products &amp; update.cy.js
+++ cypress/e2e/web-test/Choose products &amp; update.cy.js
@@ -13,8 +13,7 @@ describe('Choose product', () => {
             cy.visit('https://magento.softwaretestingboard.com/argus-all-weather-tank.html')
             cy.get('#option-label-size-143-item-168')
             cy.get('#option-label-color-93-item-52')
-            cy.get('#product-addtocart-button')
-            
+            cy.get('#product-addtocart-button')            
      })
      it(' success login without select colour', () => {
       // login
